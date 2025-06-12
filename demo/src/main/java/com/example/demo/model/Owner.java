25b757dd7d31d565d98c608415b27ee5d0739098
bbanggin <ubin.loot@gmail.com>
2025-06-12 21:00:53 +0900

    Pet 구현

--- demo/src/main/java/com/example/demo/model/Owner.java
+++ demo/src/main/java/com/example/demo/model/Owner.java
@@ -1,14 +1,18 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 public class Owner {
     private Integer id;
     private String name;
     private String password;
+    private List<Pet> pets;
 
-    public Owner(Integer id, String name, String password) {
+    public Owner(Integer id, String name, String password, List<Pet> pets) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.pets = pets;
     }
 
 
---
+++ demo/src/main/java/com/example/demo/model/Pet.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+public class Pet {
+    private Integer id;
+    private String name;
+    private String type;
+    private Integer age;
+
+    public Pet(Integer id, String name, String type, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+}
