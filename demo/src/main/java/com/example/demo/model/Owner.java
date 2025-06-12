fa9e9387513c688f3f53f7333079384ef9319ba5
bbanggin <ubin.loot@gmail.com>
2025-06-12 20:55:22 +0900

    owner 구현

---
+++ demo/src/main/java/com/example/demo/model/Owner.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+public class Owner {
+    private Integer id;
+    private String name;
+    private String password;
+
+    public Owner(Integer id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+
+}
