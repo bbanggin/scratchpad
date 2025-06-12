724e0f291d240017e604149b5da0f51b9000b038
bbanggin <ubin.loot@gmail.com>
2025-06-12 21:09:04 +0900

    OwnerController setting

---
+++ demo/src/main/java/com/example/demo/controller/OwnerController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/owner")
+@RequiredArgsConstructor
+public class OwnerController {
+
+    @RequestMapping("/info")
+    public void info() {
+        System.out.println("info");
+    }
+}
