b2c6d142c06fbedba660b4ff5d3fac24e993a17a
bbanggin <ubin.loot@gmail.com>
2025-06-12 21:04:07 +0900

    owner 정보 추가

--- demo/src/main/java/com/example/demo/model/Owner.java
+++ demo/src/main/java/com/example/demo/model/Owner.java
@@ -4,11 +4,15 @@ public class Owner {
     private Integer id;
     private String name;
     private String password;
+    private String address;
+    private String phone;
 
-    public Owner(Integer id, String name, String password) {
+    public Owner(Integer id, String name, String password, String address, String phone) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.address = address;
+        this.phone = phone;
     }
 
 
