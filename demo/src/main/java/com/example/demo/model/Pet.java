94064684259637d310bd4e1f85e21cca32e7e3e2
bbanggin <ubin.loot@gmail.com>
2025-06-13 18:23:40 +0900

    Squashed commit of the following:
    commit 7561694f4e79b28dca74c804247413aa48e829f6
    Merge: bd16233 46de9fc
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 18:22:21 2025 +0900
    
        Merge remote-tracking branch 'origin/feature/dev-3' into feature/dev-3
    
    commit bd162334a2df90ac6f8b8542526194a68aa5a217
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 18:04:26 2025 +0900
    
        수정사항 반영
    
    commit f2ad8a17c90d066131f21778d632f53cbfba1968
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 17:42:15 2025 +0900
    
        pet getter/setter 추가
    
    commit 46de9fc23a89777ae4f7d77418128156e6cdb3de
    Merge: 02bfbfe 0dac2cc
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 18:05:33 2025 +0900
    
        Merge remote-tracking branch 'origin/feature/dev-3' into feature/dev-3
    
        # Conflicts:
        #	demo/src/main/java/com/example/demo/model/Pet.java
    
    commit 02bfbfe4981738e01a225ba360037ec049341b5f
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 18:04:26 2025 +0900
    
        수정사항 반영
    
    commit e45ed2b929cb1c0397e4e410e93bb2dfaa149f74
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 17:42:15 2025 +0900
    
        pet getter/setter 추가
    
    commit fa375afa5a78776fc9195576d720e0679a6be3a7
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 17:44:39 2025 +0900
    
        pet type 추가
    
    commit 0dac2ccb7ece06f9f0ba166a0f4a35257f58c6b5
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 17:42:15 2025 +0900
    
        pet getter/setter 추가
    
    commit 4352df73f70f54dc442a7df753fe9cc3979b4fb0
    Author: bbanggin <ubin.loot@gmail.com>
    Date:   Fri Jun 13 17:38:26 2025 +0900
    
        pet 검색 추가

--- demo/src/main/java/com/example/demo/model/Pet.java
+++ demo/src/main/java/com/example/demo/model/Pet.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 
+
+import java.time.LocalDate;
+
 public class Pet {
     private Integer id;
     private String name;
     private PetType type;
     private Integer age;
+    private LocalDate birthday;
 
     public Pet(Integer id, String name, PetType type, Integer age) {
         this.id = id;
@@ -15,5 +19,25 @@ public class Pet {
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 }
